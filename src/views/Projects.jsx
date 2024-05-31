0ace0feadc744794f9cb604849fe0931dc6370d8
Antonio Turner <tonyturner1399@gmail.com>
2024-05-31 11:56:57 -0400

    changing projects page

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+// src/ProjectPage.js
 
-const Projects = () => {
+import React from 'react';
+import '../css/Projects.css';
+import { Canvas } from '@react-three/fiber';
+import { Box } from '@react-three/drei';
+
+const Projects = ({ projects }) => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="project-page">
+      {projects.map((project, index) => (
+        <div className="project-section" key={index}>
+          <h2>{project.title}</h2>
+          <div className="project-content">
+            <div className="project-info">
+              <p>{project.description}</p>
+              <a href={project.github} target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>
+            </div>
+            <div className="project-media">
+              <img src={project.image} alt={project.title} />
+            </div>
+          </div>
+          <div className="project-3d">
+            <Canvas>
+              <ambientLight />
+              <pointLight position={[10, 10, 10]} />
+              <Box>
+                <meshStandardMaterial attach="material" color="orange" />
+              </Box>
+            </Canvas>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
