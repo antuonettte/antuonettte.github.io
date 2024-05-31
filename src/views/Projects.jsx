92e114b598c842955b3c32d6f2a421e77eb80bca
Antonio Turner <tonyturner1399@gmail.com>
2024-05-31 14:56:43 -0400

    More chanages

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,23 +1,7 @@
 // src/ProjectPage.js
 
-import React, { useRef } from 'react';
+import React from 'react';
 import './ProjectPage.css';
-import { Canvas, useFrame } from '@react-three/fiber';
-
-const RotatingSphere = () => {
-  const sphereRef = useRef();
-  
-  useFrame(() => {
-    sphereRef.current.rotation.y += 0.01;
-  });
-
-  return (
-    <mesh ref={sphereRef}>
-      <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial color="white" />
-    </mesh>
-  );
-};
 
 const Projects = ({ projects }) => {
   return (
@@ -35,13 +19,6 @@ const Projects = ({ projects }) => {
             <div className="project-media">
               <img src={project.image} alt={project.title} />
             </div>
-          </div>
-          <div className="project-3d">
-            <Canvas>
-              <ambientLight />
-              <pointLight position={[10, 10, 10]} />
-              <RotatingSphere />
-            </Canvas>
           </div>
         </div>
       ))}
